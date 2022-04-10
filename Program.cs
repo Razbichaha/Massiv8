273f27c867f219b8a084a96b52c2c200791a851b
Сергей <seregabur@live.ru>
2022-04-10 10:13:29 +0300

    2

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ namespace Massiv8
 
             int[] array = new int[maximumArrayLength];
 
-            //int[] array = { 1, 2, 3, 4, 5 };
-
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = random.Next(minimumValue, maximumValue);
